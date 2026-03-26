f48bd8f330309ab65dbdc81b4ddd34677bb365b3
PierpaoloV <pierpaolo.vendittelli93@gmail.com>
2026-03-26 03:02:04 +0100

    Add SLURM interactive job script for cluster use

---
+++ code/slurm_interactive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --qos=high
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=24:00:00
+#SBATCH --output=/home/%u/logs/%j.out
+#SBATCH --no-container-entrypoint
+#SBATCH --container-mounts=/data/pathology:/data/pathology,/data/pa_cpgarchive:/data/pa_cpgarchive
+#SBATCH --container-image="pierpaolov/pathology-segmentation-pipeline:latest"
+
+mkdir -p /home/${USER}/logs
+
+jupyter lab --ip=0.0.0.0 --port=5244 --no-browser
